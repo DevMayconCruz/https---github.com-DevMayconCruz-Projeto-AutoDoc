5b7f13ce1cd3b91df40e773117ef7d878f5051cc
DevMayconCruz <dev.mayconcruz@gmail.com>
2024-10-04 17:32:50 -0300

    Adicionado mais um caminho para execução!

--- CapturaDoSistema/executarVBS.bat
+++ CapturaDoSistema/executarVBS.bat
@@ -1,9 +1,27 @@
 @echo off
-:: Define o caminho para o arquivo VBScript
-set vbsFilePath="\\Gpk-fs02\Publico\TI\Projeto-AutoDocServidor\CapturaDoSistema\getPCInfo.vbs"
+:: Define o caminho para o primeiro arquivo VBScript
+set vbsFilePath1="\\Gpk-fs02\Publico\TI\Projeto-AutoDocServidor\CapturaDoSistema\getPCInfo.vbs"
 
-:: Executa o arquivo VBScript
-cscript //nologo %vbsFilePath%
+:: Define o caminho para o segundo arquivo VBScript
+set vbsFilePath2="\\Gpk-fs02\Publico\TI\Projeto-AutoDocServidor\CapturaDoSistema\getPCInfo2.vbs"
 
-:: Fecha automaticamente a janela do prompt de comando
+:: Executa o primeiro arquivo VBScript
+cscript //nologo %vbsFilePath1%
+
+:: Verifica se o primeiro script foi executado com sucesso
+if %errorlevel% neq 0 (
+    echo Erro ao executar %vbsFilePath1%
+    exit /b %errorlevel%
+)
+
+:: Executa o segundo arquivo VBScript
+cscript //nologo %vbsFilePath2%
+
+:: Verifica se o segundo script foi executado com sucesso
+if %errorlevel% neq 0 (
+    echo Erro ao executar %vbsFilePath2%
+    exit /b %errorlevel%
+)
+
+:: Fecha automaticamente a janela do prompt de comando após executar os dois arquivos
 exit
