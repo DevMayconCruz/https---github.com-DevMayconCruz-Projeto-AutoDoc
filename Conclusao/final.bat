c8e4d512303742608b0233ccd7ca05ed2e3d7b0c
DevMayconCruz <dev.mayconcruz@gmail.com>
2024-10-05 11:03:35 -0300

    Alteração na chamada daexecução

--- Conclusao/final.bat
+++ Conclusao/final.bat
@@ -2,6 +2,10 @@
 Set WshShell = CreateObject("WScript.Shell")
 WshShell.Run "C:\caminho\para\seu\script\trocarHTA.bat", 0, False
 
+:: Exibir uma mensagem opcional enquanto aguarda 7 segundos
+echo Aguardando 7 segundos antes de fechar o HTA...
+timeout /t 7 /nobreak > NUL
+
 :: Tentativa de fechar o arquivo ContagemRegressiva.hta
 echo Tentando fechar ContagemRegressiva.hta...
 
@@ -14,7 +18,8 @@ for /f "tokens=2" %%i in ('tasklist ^| findstr /I "mshta.exe"') do (
 :: Aguardar um pouco para garantir que o arquivo foi fechado
 timeout /t 1 /nobreak > NUL
 
-:: Abrir o arquivo Final.hta
-echo Abrindo Final.hta...
-start "" "\\Gpk-fs02\Publico\TI\Projeto-AutoDocServidor\Final.hta"
+@echo off
+wscript.exe "\\Gpk-fs02\Publico\TI\Projeto-AutoDocServidor\Conclusao\final.vbs"
+
+
 
